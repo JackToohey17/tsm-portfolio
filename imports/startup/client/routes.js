70d71fe01725bae07f935494d4326155867e23e9
JackToohey17 <jtoohey12@gmail.com>
2018-02-09 13:57:22 -0500

    Set up pages

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -5,12 +5,28 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 import '../../ui/layouts/body/body.js';
 import '../../ui/pages/home/home.js';
 import '../../ui/pages/not-found/not-found.js';
+import '../../ui/pages/portfolio/portfolio.js';
+import '../../ui/pages/work/work.js';
 
 // Set up all routes in the app
 FlowRouter.route('/', {
   name: 'App.home',
   action() {
     BlazeLayout.render('App_body', { main: 'App_home' });
+  },
+});
+
+FlowRouter.route('/portfolio', {
+  name: 'portfolio',
+  action() {
+    BlazeLayout.render('App_body', { main: 'portfolio' });
+  },
+});
+
+FlowRouter.route('/work', {
+  name: 'work',
+  action() {
+    BlazeLayout.render('App_body', { main: 'work' });
   },
 });
 
---
+++ imports/ui/pages/work/work.js
@@ -0,0 +1,4 @@
+import './work.html';
+
+import '../../components/hello/hello.js';
+import '../../components/info/info.js';
