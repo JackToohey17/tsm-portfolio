a7757ea883cb9d9a3f9de3309b9571ccf4497944
JackToohey17 <jtoohey12@gmail.com>
2018-03-28 14:06:12 -0400

    Added iphoneX image wrapper

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -10,7 +10,29 @@ const seedPortfolios = (count = 10) => {
   if (seedCount > 0) seedPortfolio(seedCount);
 };
 
+const portfolios = [{
+  title: 'Total Request Now',
+  description: 'Accusamus sit eos ut quae.\nUt laborum magni voluptatem.\nOdit sapiente perferendis voluptatem doloribus dolores.\nVoluptas eos qui ad enim.\n \rDoloremque dolor minus qui sit aut corrupti at.\nAtque porro quasi deleniti provident non velit doloremque vel vero.\nTenetur qui aliquid at repellendus.\nEum in corrupti.\nQui architecto accusantium eum facilis.\n \rUt a iusto architecto qui vero quisquam quas quia ratione.\nEt ut perferendis voluptas quis cupiditate id ea eius numquam.\nSed porro nemo voluptas qui.\nVel dolorum magnam qui aspernatur sed temporibus sit illum.\nConsequatur omnis velit odit qui sed ut dolores maiores harum.\nNesciunt ut labore recusandae.',
+  link: 'http://totalrequestnow-testing.herokuapp.com/rd-app-view',
+  img: 'https://s3.amazonaws.com/myfangate.com/rdvote/trnow.png',
+  endDate: Factory.tree('portfolio').endDate,
+}, {
+  title: 'Which Are You',
+  description: 'Accusamus sit eos ut quae.\nUt laborum magni voluptatem.\nOdit sapiente perferendis voluptatem doloribus dolores.\nVoluptas eos qui ad enim.\n \rDoloremque dolor minus qui sit aut corrupti at.\nAtque porro quasi deleniti provident non velit doloremque vel vero.\nTenetur qui aliquid at repellendus.\nEum in corrupti.\nQui architecto accusantium eum facilis.\n \rUt a iusto architecto qui vero quisquam quas quia ratione.\nEt ut perferendis voluptas quis cupiditate id ea eius numquam.\nSed porro nemo voluptas qui.\nVel dolorum magnam qui aspernatur sed temporibus sit illum.\nConsequatur omnis velit odit qui sed ut dolores maiores harum.\nNesciunt ut labore recusandae.',
+  link: 'https://tsm-which-are-you.herokuapp.com/',
+  img: 'http://placekitten.com/200/300',
+  endDate: Factory.tree('portfolio').endDate,
+}];
+
+const seedPortfolioByTitle = (portfolio) => {
+  const portfolioExists = Portfolios.findOne({title: portfolio.title});
+  if (!portfolioExists) {
+    Portfolios.insert(portfolio);
+  }
+}
+
 const seedPortfolio = (count = 1) => {
+  portfolios.forEach(seedPortfolioByTitle);
   for (let i = 0; i < count ; i += 1) Factory.create('portfolio');
 };
 
