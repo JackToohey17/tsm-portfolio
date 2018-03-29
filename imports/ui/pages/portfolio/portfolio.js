5a8ea057c9f45528e6eec1c9815ef05403846f5e
mqschwanda <mqschwanda@gmail.com>
2018-03-29 16:51:35 -0400

    update iframe

--- imports/ui/pages/portfolio/portfolio.js
+++ imports/ui/pages/portfolio/portfolio.js
@@ -1,17 +1,13 @@
-import {Template} from 'meteor/templating';
-import {Portfolios} from '../../../api/portfolios/portfolios.js';
-import {FlowRouter} from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+import { Portfolios } from '../../../api/portfolios/portfolios.js';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 
 import './portfolio.html';
 
 
-Template.portfolio.onCreated(function() {
-  // console.log(this);
-});
+Template.portfolio.onCreated(function() {});
 
-Template.portfolio.onRendered(function() {
-  // console.log(this);
-});
+Template.portfolio.onRendered(function() {});
 
 Template.portfolio.helpers({
   portfolio: function() {
@@ -19,23 +15,4 @@ Template.portfolio.helpers({
   },
 });
 
-const handleIframeScroll = (event) => {
-  console.log("handleIframeScroll", event);
-  const $iframe = $(event.target).children("iframe");
-  const scrollTop = $iframe.scrollTop();
-  const {wheelDeltaY} = event;
-  // $iframe.scrollTop(wheelDeltaY + scrollTop);
-  document.getElementById('iframe').contentWindow.scrollTo(wheelDeltaY + scrollTop);
-  console.log(scrollTop, $iframe.scrollTop(), wheelDeltaY, $iframe);
-}
-
-Template.portfolio.events({
-  'mouseover .iframe-wrapper': (event, template) => {
-    console.log("mouseover", event);
-    window.addEventListener("wheel", handleIframeScroll);
-  },
-  'mouseout .iframe-wrapper': (event, template) => {
-    console.log("mouseout", event);
-    window.removeEventListener("wheel", handleIframeScroll);
-  }
-})
+// Template.portfolio.events({})
